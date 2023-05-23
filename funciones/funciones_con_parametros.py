587a137fb16ef29ef153f28eadc70ca285ff9098
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 19:45:17 -0300

    Update_funciones_con_parametros.py

--- funciones/funciones_con_parametros.py
+++ funciones/funciones_con_parametros.py
@@ -1,26 +1,31 @@
 def resta (a, b):
-    print (a-b)
+    #print (a-b)
+    #en lugar de print DEVOLVER usando return
+    return a-b
 
 resultado=resta(5, 2)
 print(resultado)
 
 
 def pares (numero):
-    print (numero % 2 == 0)
+   # print (numero % 2 == 0)
+    return numero % 2 == 0
 
 resultado=pares (7)
 print(resultado)
 
 
 def es_bisiesto(anio):
-    print (anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0))
+   # print (anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0))
+    return  (anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0))
 
 resultado=es_bisiesto(2016)
 print(resultado)
 
 
 def convertir_mayusculas(cadena):
-    print (cadena.upper())
+    #print (cadena.upper())
+    return cadena.upper()
 
 resultado=convertir_mayusculas("meliana")
 print(resultado)
@@ -28,11 +33,14 @@ print(resultado)
 
 def verificar_signo(numero):
     if numero > 0:
-        print ("Positivo")
+       # print ("Positivo")
+        return "Positivo"
     elif numero < 0:
-        print ("Negativo")
+        #print ("Negativo")
+        return "Negativo"
     else:
-        print ("Cero")
+       # print ("Cero")
+       return "Cero"
 
 resultado=verificar_signo(-8)
 print(resultado)
