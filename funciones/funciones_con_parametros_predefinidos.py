f3597a5a6b2b4f8ac2820993f93a3f5a1296ad88
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 19:50:24 -0300

    Update_funciones_con_parametros_predefinidos.py

--- funciones/funciones_con_parametros_predefinidos.py
+++ funciones/funciones_con_parametros_predefinidos.py
@@ -1,12 +1,14 @@
 def potencia(base=10, exponente=2):
-    print (base ** exponente)
+    #print (base ** exponente)
+    return (base ** exponente)
 
 resultado=potencia()
 print(resultado)
 
 
 def maximo(a=15, b=12, c=1):
-    print (max(a, b, c))
+    #print (max(a, b, c))
+    return  (max(a, b, c))
 
 resultado=maximo()
 print(resultado)
@@ -16,19 +18,21 @@ def mostrar_rango(inicio=4, fin=9):
     for numero in range(inicio, fin+1):
         print(numero)
 
-resultado=mostrar_rango()
-print(resultado)
-
+#resultado=mostrar_rango()
+#print(resultado)
+mostrar_rango()
 
 def decimal_a_binario(decimal=10):
-    print (bin(decimal)[2:])
+    #print (bin(decimal)[2:])
+    return (bin(decimal)[2:])
 
 resultado=decimal_a_binario()
 print(resultado)
 
 
 def area_circulo(radio=4):
-    print (3.14159265359 * (radio ** 2))
+    #print (3.14159265359 * (radio ** 2))
+    return 3.14159265359 * (radio ** 2)
 
 resultado=area_circulo()
 print(resultado)
