ef65589ed8855be95b64a14d32d1f89df0264c42
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 19:53:14 -0300

    Update_funciones_sin_parametros.py

--- funciones/funciones_sin_parametros.py
+++ funciones/funciones_sin_parametros.py
@@ -1,34 +1,38 @@
 def saludos ():
-    print ("buen dia")
+    #print ("buen dia")
+    return "buen dia"
 
 resultado=saludos()
 print(resultado)
 
 
 def poema ():
-    print ("Por más que cante un pájaro en el bosque, el ciruelo no rompe a florecer - Aiku")
+     return ("Por más que cante un pájaro en el bosque, el ciruelo no rompe a florecer - Aiku")
 
 resultado= poema()
 print(resultado)
 
 
 def nombre_edad ():
-    print (" Meliana, 26 años")
+    return(" Meliana, 26 años")
 
 resultado= nombre_edad ()
 print(resultado)
 
 
 def ciudad ():
-    print ("Malagueño")
+    return ("Malagueño")
 
 resultado=ciudad()
 print(resultado)
 
 
 def mostrar_numeros_impares():
+    impares = []
     for numero in range(1, 21, 2):
-        print(numero)
+        #print(numero)
+        impares.append(numero)
+     return impares
 
 resultado=mostrar_numeros_impares()
 print(resultado)
